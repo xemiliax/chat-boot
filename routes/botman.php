4b009842c437d782e43deb3508e5c7b180ef3b56
Ingrid Emilia <ingrid-emis@hotmail.com>
2020-07-06 16:22:55 -0300

    start conversation

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,11 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$botman->hears('Olá', function($bot) {
+    $bot->reply('Olá! Tudo bem?');
+    $bot->ask('Qual seu nome? Em que posso ajuda-lo?', function($answer, $bot) {
+        $bot->say('Bem vindo! '.$answer->getText());
+    });
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->listen();
